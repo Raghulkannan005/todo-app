92eaa1e17b9af541b4b88263fa8f91484380e781
Raghulkannan005 <raghulkannan005@gmail.com>
2024-11-24 11:44:41 +0530

    todo-app backend bone-structure

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+
+app.get('/todos',function (req, res){
+
+})
+
+app.post('/todo',function (req, res){
+
+})
+
+
+app.post('/completed',function (req, res){
+
+})
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000!")
+})

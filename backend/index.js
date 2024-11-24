4cc0fb6117b72f38e6883e72f61cf7b378faef02
Raghulkannan005 <raghulkannan005@gmail.com>
2024-11-24 14:14:52 +0530

    done with backend

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,63 @@
 const express = require('express')
+const {createTodo, updateTodo} = require('./types')
+const {todo} = require('./db')
 
 const app = express()
 app.use(express.json())
 
-app.get('/todos',function (req, res){
+app.post('/todo', async function (req, res){
+    const createPayload = req.body
+    const parsedPayload = createTodo.safeParse(createPayload)
+
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg : "You have sent the wrong input"
+        })
+        return
+    }
+    await todo.create({
+        title : createPayload.title,
+        description : createPayload.description,
+        completed : todo.completed
+    })
+    res.json({
+        msg : "Todo created successfully"
+    })
+})
+
+app.get('/todos', async function (req, res){
+
+    const todos = await todo.find({})
+    res.json({
+        todos : todos.map((todo) => {
+            return {
+                title : todo.title,
+                description : todo.description,
+                completed : todo.completed
+            }
+        })
+    })
 
 })
 
-app.post('/todo',function (req, res){
+app.post('/completed', async function (req, res){
+    const updatePayload = req.body
+    const parsePayload = updateTodo.safeParse(updatePayload)
 
-})
-
-
-app.post('/completed',function (req, res){
+    if(!parsePayLoad.success){
+        res.status(411).json({
+            msg : "you have sent the wrong id"
+        })
+        return
+    }
+    await todo.update({
+        _id: req.body.id
+    },{
+        completed : true
+    })
+    res.json({
+        msg : "Todo marked as completed"
+    })
 
 })
 
